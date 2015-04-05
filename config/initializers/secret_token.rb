bb2d68ab3a80adae4847b5af55f45be9a6d7a2e8
NeverSmileK57CLC <neversmile12b1@gmail.com>
2015-04-05 11:56:47 +0700

    rails project postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondRailsPg::Application.config.secret_key_base = 'aa6a1fd38698ce20a46ee418085f2ddefbe9a461ad2adbd5c627480bac2a64790748924b8e467a9d03a96db5157e44e5d218a59db860ae63d1ad99db0a827e78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SecondRailsPg::Application.config.session_store :cookie_store, key: '_second_rails_pg_session'
